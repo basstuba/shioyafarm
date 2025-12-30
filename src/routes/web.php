85bdce3ed53f1b2c96fd0506dd7e53238812c825
nutka <tuba4080@outlook.com>
2025-12-30 20:59:48 +0100

    feat: 送料案内・利用規約・プライバシーポリシー・特商法各ページのルート作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 送料案内
+Route::get('/shipping-fee', function () {
+    return view('shipping');
+})->name('shipping-fee');
+
+// 利用規約
+Route::get('/terms-of-service', function () {
+    return view('terms-of-service');
+})->name('terms-of-service');
+
+// プライバシーポリシー
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+})->name('privacy-policy');
+
+// 特定商取引法に基づく表記
+Route::get('/commercial-transaction', function () {
+    return view('law');
+})->name('commercial-transaction');
