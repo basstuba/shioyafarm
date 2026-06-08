cb5226dcd7101432c798e5fe380b7fe92464ff7e
nutka <tuba4080@outlook.com>
2026-06-08 16:09:07 +0200

    feat: 特商法ページのベースコード作成

---
+++ src/resources/views/law.blade.php
@@ -0,0 +1,80 @@
+@section('layouts.app')
+
+@section('body-class', 'legal-page')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/law.css') }}">
+@endsection
+
+@section('content')
+    <div class="law">
+        <div class="container">
+            <h2 class="title">特定商取引法に基づく表記</h2>
+            <hr class="divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd>
+                    <p></p>
+                    <p></p>
+                </dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd></dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd>
+                    <p></p>
+                    <p></p>
+                </dd>
+            </section>
+            <hr class="section-divider">
+            <section class="section">
+                <dt></dt>
+                <dd>
+                    <p></p>
+                    <p></p>
+                </dd>
+            </section>
+            <hr class="section-divider">
+        </div>
+    </div>
+@endsection
