3938670ecb912f1ca66cf4de657582aae7d8f4f7
nutka <tuba4080@outlook.com>
2026-06-15 10:04:57 +0200

    fix: 配送料金一覧テーブル_configを使用したコードに修正

--- src/resources/views/shipping/fee-list.php
+++ src/resources/views/shipping/fee-list.php
@@ -2,217 +2,36 @@
     <table class="fee-list">
         <thead>
             <tr>
-                <th scope="col" class="fee-list__title">お届け先</th>
-                <th scope="col" class="fee-list__title">北海道</th>
-                <th scope="col" class="fee-list__title">北東北</th>
-                <th scope="col" class="fee-list__title">南東北</th>
-                <th scope="col" class="fee-list__title">関東</th>
-                <th scope="col" class="fee-list__title">信越</th>
-                <th scope="col" class="fee-list__title">北陸</th>
-                <th scope="col" class="fee-list__title">中部</th>
-                <th scope="col" class="fee-list__title">関西</th>
-                <th scope="col" class="fee-list__title">中国</th>
-                <th scope="col" class="fee-list__title">四国</th>
-                <th scope="col" class="fee-list__title">九州</th>
-                <th scope="col" class="fee-list__title">沖縄</th>
+                <th scope="col" class="area">お届け先</th>
+
+                @foreach(config('shipping.areas') as $area => $prefectures)
+                    <th scope="col" class="area__name">{{ $area }}</th>
+                @endforeach
             </tr>
         </thead>
         <tbody>
             <tr>
-                <th scope="row" class="area">&nbsp;</th>
-                <td class="area__name">北海道</td>
-                <td class="area__name">
-                    <ul>
-                        <li>青森県</li>
-                        <li>秋田県</li>
-                        <li>岩手県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>宮城県</li>
-                        <li>山形県</li>
-                        <li>福島県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>茨城県</li>
-                        <li>栃木県</li>
-                        <li>群馬県</li>
-                        <li>埼玉県</li>
-                        <li>千葉県</li>
-                        <li>神奈川県</li>
-                        <li>東京都</li>
-                        <li>山梨県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>新潟県</li>
-                        <li>長野県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>富山県</li>
-                        <li>石川県</li>
-                        <li>福井県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>静岡県</li>
-                        <li>愛知県</li>
-                        <li>三重県</li>
-                        <li>岐阜県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>大阪府</li>
-                        <li>京都府</li>
-                        <li>滋賀県</li>
-                        <li>奈良県</li>
-                        <li>和歌山県</li>
-                        <li>兵庫県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>岡山県</li>
-                        <li>広島県</li>
-                        <li>山口県</li>
-                        <li>鳥取県</li>
-                        <li>島根県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>香川県</li>
-                        <li>徳島県</li>
-                        <li>愛媛県</li>
-                        <li>高知県</li>
-                    </ul>
-                </td>
-                <td class="area__name">
-                    <ul>
-                        <li>福岡県</li>
-                        <li>佐賀県</li>
-                        <li>長崎県</li>
-                        <li>熊本県</li>
-                        <li>大分県</li>
-                        <li>宮崎県</li>
-                        <li>鹿児島県</li>
-                    </ul>
-                </td>
-                <td class="area__name">沖縄県</td>
+                <td scope="row" class="space">&nbsp;</td>
+
+                @foreach(config('shipping.areas') as $area => $prefectures)
+                    <td class="prefecture__name">
+                        <ul>
+                            @foreach($prefectures as $prefecture)
+                                <li>{{ $prefecture }}</li>
+                            @endforeach
+                        </ul>
+                    </td>
+                @endforeach
             </tr>
-            <tr>
-                <th scope="row" class="division-type">60サイズ</th>
-                <td class="division__fee">940</td>
-                <td class="division__fee">1,190</td>
-                <td class="division__fee">1,320</td>
-                <td class="division__fee">1,460</td>
-                <td class="division__fee">1,460</td>
-                <td class="division__fee">1,610</td>
-                <td class="division__fee">1,610</td>
-                <td class="division__fee">1,920</td>
-                <td class="division__fee">2,070</td>
-                <td class="division__fee">2,070</td>
-                <td class="division__fee">2,340</td>
-                <td class="division__fee">2,340</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">80サイズ</th>
-                <td class="division__fee">1,230</td>
-                <td class="division__fee">1,460</td>
-                <td class="division__fee">1,610</td>
-                <td class="division__fee">1,740</td>
-                <td class="division__fee">1,740</td>
-                <td class="division__fee">1,900</td>
-                <td class="division__fee">1,900</td>
-                <td class="division__fee">2,200</td>
-                <td class="division__fee">2,360</td>
-                <td class="division__fee">2,360</td>
-                <td class="division__fee">2,620</td>
-                <td class="division__fee">2,950</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">100サイズ</th>
-                <td class="division__fee">1,530</td>
-                <td class="division__fee">1,790</td>
-                <td class="division__fee">1,920</td>
-                <td class="division__fee">2,050</td>
-                <td class="division__fee">2,050</td>
-                <td class="division__fee">2,200</td>
-                <td class="division__fee">2,200</td>
-                <td class="division__fee">2,510</td>
-                <td class="division__fee">2,670</td>
-                <td class="division__fee">2,670</td>
-                <td class="division__fee">2,930</td>
-                <td class="division__fee">3,590</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">120サイズ</th>
-                <td class="division__fee">1,850</td>
-                <td class="division__fee">2,110</td>
-                <td class="division__fee">2,240</td>
-                <td class="division__fee">2,370</td>
-                <td class="division__fee">2,370</td>
-                <td class="division__fee">2,520</td>
-                <td class="division__fee">2,520</td>
-                <td class="division__fee">2,830</td>
-                <td class="division__fee">2,990</td>
-                <td class="division__fee">2,990</td>
-                <td class="division__fee">3,250</td>
-                <td class="division__fee">4,240</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">140サイズ</th>
-                <td class="division__fee">2,190</td>
-                <td class="division__fee">2,450</td>
-                <td class="division__fee">2,580</td>
-                <td class="division__fee">2,710</td>
-                <td class="division__fee">2,710</td>
-                <td class="division__fee">2,860</td>
-                <td class="division__fee">2,860</td>
-                <td class="division__fee">3,170</td>
-                <td class="division__fee">3,330</td>
-                <td class="division__fee">3,330</td>
-                <td class="division__fee">3,590</td>
-                <td class="division__fee">4,910</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">160サイズ</th>
-                <td class="division__fee">2,510</td>
-                <td class="division__fee">2,770</td>
-                <td class="division__fee">2,900</td>
-                <td class="division__fee">3,030</td>
-                <td class="division__fee">3,030</td>
-                <td class="division__fee">3,180</td>
-                <td class="division__fee">3,180</td>
-                <td class="division__fee">3,490</td>
-                <td class="division__fee">3,650</td>
-                <td class="division__fee">3,650</td>
-                <td class="division__fee">3,910</td>
-                <td class="division__fee">5,560</td>
-            </tr>
-            <tr>
-                <th scope="row" class="division-type">180サイズ</th>
-                <td class="division__fee">3,060</td>
-                <td class="division__fee">4,090</td>
-                <td class="division__fee">4,220</td>
-                <td class="division__fee">4,350</td>
-                <td class="division__fee">4,350</td>
-                <td class="division__fee">5,380</td>
-                <td class="division__fee">5,380</td>
-                <td class="division__fee">5,690</td>
-                <td class="division__fee">6,180</td>
-                <td class="division__fee">6,180</td>
-                <td class="division__fee">6,550</td>
-                <td class="division__fee">9,080</td>
-            </tr>
+            @foreach(config('shipping.fees') as $size => $fees)
+                <tr>
+                    <th scope="row" class="division-type">{{ $size }}サイズ</th>
+
+                    @foreach($fees as $fee)
+                        <td class="division__fee">{{ number_format($fee) }}</td>
+                    @endforeach
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
